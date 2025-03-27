01c5d8895fabdc03812c5dc66fc5dde4995b9740
Demo_hai <abhishekshukla12357@gmail.com>
2025-03-27 17:28:45 +0530

    Fixed Vite base path issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/OnlineBussness/", // GitHub repo name
+  base: "/OnlineBussness/", // Repo name ke hisaab se yeh set karna zaroori hai!
 });
